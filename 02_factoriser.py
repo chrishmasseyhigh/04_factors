7ac70e96dc2562f639d731f8aa1f8f2c1f8a2605
chrishmasseyhigh <87159815+chrishmasseyhigh@users.noreply.github.com>
2021-09-13 19:45:23 +1200

    x

--- 02_factoriser.py
+++ 02_factoriser.py
@@ -1,3 +1,25 @@
+import math 
+
+
 # gets factors, returns a sorted list
 def get_factors(to_factor):
+    if to_factor == 1:
+        return [1]
+
+    my_list = []
+
+    max_factor = round(math.sqrt(to_factor))
+
+    for item in range(1, max_factor+1):
+        my_list.append(item)
+
+    return my_list
+
+
     
+
+
+print(get_factors(1))
+print(get_factors(12))
+print(get_factors(24))
+
